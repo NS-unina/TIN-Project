d4613e150b99ba4923aac9445a215f5f7e1f5a8e
marika <marik.sasso@studenti.unina.it>
2024-11-16 13:48:30 +0100

    add list of vms with related informations

--- server_flask/function_server.py
+++ server_flask/function_server.py
@@ -9,9 +9,6 @@ import requests
 #Directory for vms
 VM_PATH = './vm'
 
-#Dictionary
-vm_id_dictionary = {}
-
 #OVS 
 ovs_bridge = "br0"
 
@@ -24,6 +21,7 @@ def create_vagrantfile(vagrantfile_path,name,box,cpus,ram,ip,tap):
     vagrantfile_content= f"""
     Vagrant.configure("2") do |config|
         config.vm.box = "{box}"
+        config.vm.hostname = "{name}"
 
         config.vm.provider "virtualbox" do |vb|
             vb.memory = {ram}
@@ -113,57 +111,125 @@ def update_ip(ip, vm_name):
 
 
 #Define vm's id
-def generate_unique_id(vm_name, length=5):
+def generate_unique_id(length=5):
     
     try:
-        with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'r') as id_list:
-            vm_id_dictionary = json.load(id_list)
-    except FileNotFoundError as e:
-        return jsonify ({'error': f'{e}'}), 500
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as vm_list:
+            vm_dictionary = json.load(vm_list)
+    except FileNotFoundError:
+        return jsonify ({'error': f"VM_list doesn't exists."}), 404
 
     while True:
 
         vm_id = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-        if vm_id not in vm_id_dictionary:
-            vm_id_dictionary[vm_name] = vm_id
-        
-            with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'w') as id_list:
-                id_list.write(json.dumps(vm_id_dictionary))
-        
-        return vm_id
+        if vm_id not in vm_dictionary:
+            return vm_id
 
 
-#Delete from dictionary - vm_id
-def delete_from_dictionary(vm_name):
-    
-    try:
-        with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'r') as id_list:
-            vm_id_dictionary = json.load(id_list)
-    except FileNotFoundError as e:
-        return jsonify ({'error': f'{e}'}), 500
 
-    if vm_name in vm_id_dictionary:
-        del vm_id_dictionary[vm_name]
-    
-        with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'w') as id_list:
-            id_list.write(json.dumps(vm_id_dictionary))
+# ********[DICTIONARY LIST VM]********
 
-
+#Init dictionary
 def init_int():
-    vm_id_dictionary = {}
+    vm_dictionary = {}
     
     #Read from id_list already existing interfaces
     try:
-        with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'r') as id_list:
-            vm_id_dictionary = json.load(id_list)
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as id_list:
+            vm_dictionary = json.load(id_list)
     except FileNotFoundError:
-        id_list = open(os.path.join(VM_PATH, 'ID_LIST.json'), 'w')
+        id_list = open(os.path.join(VM_PATH, 'VM_list.json'), 'w')
         id_list.write(json.dumps({}))
         id_list.close()
     except json.JSONDecodeError as err:
         print (err)
     
-    return vm_id_dictionary
+    #Request creating already existing interfaces
+    for key, value in vm_dictionary.items():
+        veth_name = value.get("id")
+        try:
+            url= f"{NET_SERVER}/network/create_int/{veth_name}"
+            response=requests.post(url,json="")
+            print (response.json())
+        except Exception as e:
+            print (jsonify({"error": str(e)}), 400)
 
+    return vm_dictionary
+
+
+# Create item in vm dictionary
+def create_item_vm_list(vm_name, id, ram, cpu, ip):
+
+    try:
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as vm_list:
+            vm_dictionary = json.load(vm_list)
+    except FileNotFoundError as e:
+        return jsonify ({'error': f"VM_list doesn't exists."}), 404
+
+    vm_dictionary[vm_name] = {
+        "id": id,
+        "ram": ram,
+        "cpu": cpu,
+        "ip": ip,
+        #"status": status
+    }
+
+    with open(os.path.join(VM_PATH, 'VM_list.json'), 'w') as vm_list:
+        vm_list.write(json.dumps(vm_dictionary))
     
+
+# Update item in vm dictionary
+def update_item_vm_list(vm_name, field, value_field):
+
+    try:
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as vm_list:
+            vm_dictionary = json.load(vm_list)
+    except FileNotFoundError as e:
+        return jsonify ({'error': f"VM_list doesn't exists."}), 404
+
+    if vm_name in vm_dictionary:
+        if field in vm_dictionary[vm_name]:
+            vm_dictionary[vm_name][field] = value_field
+        else:
+           return jsonify({'error': f"Field '{field}' doesn't exists."}), 404
+    else:
+        return jsonify({'error': f"VM '{vm_name}' doesn't exists."}), 404
+    
+    with open(os.path.join(VM_PATH, 'VM_list.json'), 'w') as vm_list:
+        vm_list.write(json.dumps(vm_dictionary))
+
+    return jsonify({'message': f"Field '{field}' successfully update to {value_field}."}), 200
+
+#Search value of a field in dictionary
+def search_item_vm_list(vm_name, field):
+
+    try:
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as vm_list:
+            vm_dictionary = json.load(vm_list)
+    except FileNotFoundError as e:
+        return jsonify ({'error': f"VM_list doesn't exists."}), 404
+
+    if vm_name in vm_dictionary:
+        if field in vm_dictionary[vm_name]:
+            return vm_dictionary[vm_name][field]
+        else:
+           return jsonify({'error': f"Field '{field}' doesn't exists."}), 404
+    else:
+        return jsonify({'error': f"VM '{vm_name}' doesn't exists."}), 404
+
+#Delete from dictionary
+def delete_from_dictionary(vm_name):
+    
+    try:
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'r') as vm_list:
+            vm_dictionary = json.load(vm_list)
+    except FileNotFoundError as e:
+        return jsonify ({'error': f"VM_list doesn't exists."}), 404
+
+    if vm_name in vm_dictionary:
+        del vm_dictionary[vm_name]
+    
+        with open(os.path.join(VM_PATH, 'VM_list.json'), 'w') as vm_list:
+            vm_list.write(json.dumps(vm_dictionary))
+
--- server_flask/vm_configurator.py
+++ server_flask/vm_configurator.py
@@ -1,4 +1,3 @@
-from time import sleep
 from function_server import *
 from flask_cors import CORS
 import vagrant
@@ -11,19 +10,10 @@ app.config.from_object(__name__)
 CORS(app,resources={r'/*':{'origins':'*'}})
 
 
-
-#Richiesta automatica di init
+#Request init list of vm and creations of interfaces
 vm_id_dictionary = init_int()
 print (vm_id_dictionary)
 
-for values in vm_id_dictionary:
-    veth_name = vm_id_dictionary.get(values)
-    try:
-        url= f"{NET_SERVER}/network/create_int/{veth_name}"
-        response=requests.post(url,json="")
-        print (response.json())
-    except Exception as e:
-        print (jsonify({"error": str(e)}), 400)
 
 
 @app.route('/create', methods=['POST'])
@@ -35,21 +25,23 @@ def create_vm():
     vm_ram = data.get ('ram', 1024) #default 1024 MB
     vm_ip=data.get('ip')
 
-
+    #Field needed
     if not vm_name:
         return jsonify({"error": "name field is needed"}), 400
-    
     if not vm_ip:
         return jsonify({"error": "ip field is needed"}), 400
     
     #Check if vm already exist
     vm_path = os.path.join(VM_PATH, vm_name)
     if os.path.exists(vm_path):
-        raise FileExistsError()
+        return jsonify({"error": f"VM '{vm_name}' already exist"}), 404
+    
+    #Create directory if not exists
+    vm_path = os.path.join(VM_PATH, vm_name)
     os.makedirs(vm_path)
 
     #Generate vm_id
-    vm_id = generate_unique_id(vm_name)
+    vm_id = generate_unique_id()
 
     #Create network interfaces for the VM
     try:
@@ -71,35 +63,31 @@ def create_vm():
     except FileExistsError as e:
         return jsonify({"error": f"VM '{vm_name}' already exist. To modify it use the update request."}), 400 
 
+    #Save information in vm dictionary
+    create_item_vm_list(vm_name, vm_id, vm_ram, vm_cpus, vm_ip)
+
     #Starting the vm
     try:
         v = vagrant.Vagrant(vm_path)
         #v.up()
         return jsonify({"message": f"VM '{vm_name}' successfully created!"}), 201
-    except Exception:
-        return jsonify({"error": "Error deleting vm"}), 500
+    except Exception as e:
+        return jsonify({"error": f"Error deleting vm. {e}"}), 500
 
 
 @app.route('/delete/<vm_name>', methods=['DELETE'])
 def delete_vm(vm_name):
 
-    #Check if vm exists
+    #Check if vm already exist
     vm_path = os.path.join(VM_PATH, vm_name)
     if not os.path.exists(vm_path):
         return jsonify({"error": f"VM '{vm_name}' doesn't exist"}), 404
-    
+
     #Find associated interface
-    try:
-        with open(os.path.join(VM_PATH, 'ID_LIST.json'), 'r') as id_list:
-            vm_id_dictionary = json.load(id_list)
-    except FileNotFoundError as e:
-        return jsonify ({'error': f'{e}'}), 500
-    
-    print (vm_id_dictionary)
-    vm_id = vm_id_dictionary.get(vm_name)
-    print (vm_id)
+    vm_id = search_item_vm_list(vm_name, "id")
 
     try:
+        #Delete network interfaces for the VM
         url= f"{NET_SERVER}/network/delete_int/{vm_id}"
         response=requests.delete(url)
 
@@ -109,17 +97,14 @@ def delete_vm(vm_name):
         else:
             return jsonify({"error": f"Request failed with status code: {response.status_code}"})
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
-    #Deleting vm and directory
-    try:
+        #Deleting vm and directory
         v = vagrant.Vagrant(vm_path)
         #v.destroy()
         shutil.rmtree(vm_path)
         return jsonify({"message": f"VM '{vm_name}' sucessfully deleted!"}), 200
-    except Exception:
-        return jsonify({"error": "Error deleting vm"}), 500
+    
+    except Exception as e:
+        return jsonify({"error": f"Error deleting vm. {e}"}), 500
 
 
 @app.route('/read', methods=['GET'])
@@ -155,9 +140,9 @@ def update_vm(vm_name):
     vm_ip=data.get('ip')
 
     if ((not vm_cpus) and (not vm_ram) and (not vm_ip)):
-        return jsonify({"error": "Field is needed"}), 400
+        return jsonify({"error": "At least one field of 'cpus', 'ram' or 'ip' is needed"}), 400
     
-    #Check if vm exist
+    #Check if vm already exist
     vm_path = os.path.join(VM_PATH, vm_name)
     if not os.path.exists(vm_path):
         return jsonify({"error": f"VM '{vm_name}' doesn't exist"}), 404
@@ -165,14 +150,15 @@ def update_vm(vm_name):
     try:
         if (vm_cpus):
             update_cpu(vm_cpus, vm_name)
-
+            update_item_vm_list(vm_name, "cpu", vm_cpus)
         if (vm_ram):
             update_ram(vm_ram, vm_name)  
-        
+            update_item_vm_list(vm_name, "ram", vm_ram)
         if (vm_ip):
             update_ip(vm_ip, vm_name)
+            update_item_vm_list(vm_name, "ip", vm_ip)
     except FileNotFoundError:
-        return jsonify({"error": f" Vagrantfile for'{vm_name}' doesn't exist"}), 404
+        return jsonify({"error": f" Vagrantfile for '{vm_name}' doesn't exist"}), 404
 
     try:
         v = vagrant.Vagrant(vm_path)
@@ -191,12 +177,13 @@ def power_start_vm(vm_name):
     if not os.path.exists(vm_path):
         return jsonify({"error": f"VM '{vm_name}' doesn't exist"}), 404
 
+    #Starting the vm
     try:
         v = vagrant.Vagrant(vm_path)
         v.up()
         return jsonify({"message": f"VM '{vm_name}' successfully started!"}), 201
-    except Exception:
-        return jsonify({"error": "Error starting  vm"}), 500
+    except Exception as e:
+        return jsonify({"error": f"Error starting  vm. {e}"}), 500
 
 
 @app.route('/stop/<vm_name>', methods=['POST'])
@@ -212,7 +199,7 @@ def power_stop_vm(vm_name):
         v.halt()
         return jsonify({"message": f"VM '{vm_name}' successfully stopped!"}), 201
     except Exception:
-        return jsonify({"error": "Error stopping  vm"}), 500
+        return jsonify({"error": f"Error stopping  vm. {e}"}), 500
 
 
 
